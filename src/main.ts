5d7af71e566c5723eb01a77cdf5a9c3acc52d26d
@shadowinhaze <shadowinhaze@gmail.com>
2022-07-15 22:56:50 +0200

    minor: remove unnecesary entities

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(port, () => {
+    console.log(`Server started on ${port} port.`);
+  });
 }
+
 bootstrap();
