23018d92b6fdc26a081d88260f27ead1040cde89
@shadowinhaze <shadowinhaze@gmail.com>
2022-07-17 19:35:34 +0200

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const document = SwaggerModule.createDocument(app, config);
-
   app.useGlobalPipes(new ValidationPipe());
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port, () => {
     console.log(`Server started on ${port} port.`);
